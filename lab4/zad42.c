bdce07414daa5bfd088720d0c59ea1ce84d35936
Hbrtjm <hubi.hey@vp.pl>
2024-03-27 23:48:56 +0100

    Working on task 4

--- lab4/zad42.c
+++ lab4/zad42.c
@@ -11,23 +11,22 @@
 #define MAX_ID_LEN 64
 #define MAX_IDS 1024
 
-#define TEST 1  // 1 - dla testowania,  0 - dla automatycznej oceny
+#define TEST 1 // 1 - dla testowania,  0 - dla automatycznej oceny
 
-int index_cmp(const void*, const void*);
-int cmp(const void*, const void*);
+int index_cmp(const void *, const void *);
+int cmp(const void *, const void *);
 
 char tab[MAX_IDS][MAX_ID_LEN];
 
 char *keywords[] = {
-	"auto", "break", "case", "char",
-	"const", "continue", "default", "do",
-	"double", "else", "enum", "extern",
-	"float", "for", "goto", "if",
-	"int", "long", "register", "return",
-	"short", "signed", "sizeof", "static",
-	"struct", "switch", "typedef", "union",
-	"unsigned", "void", "volatile", "while"
-};
+    "auto", "break", "case", "char",
+    "const", "continue", "default", "do",
+    "double", "else", "enum", "extern",
+    "float", "for", "goto", "if",
+    "int", "long", "register", "return",
+    "short", "signed", "sizeof", "static",
+    "struct", "switch", "typedef", "union",
+    "unsigned", "void", "volatile", "while"};
 
 int is_alfa(char a)
 {
@@ -44,15 +43,27 @@ int is_alfanum(char a)
     return is_alfa(a) || is_num(a);
 }
 
-int valid(char word[],int length)
+int is_keyword(char word[])
 {
-    if(!is_alfa(word[0]))
+    for (int i = 0; i < MAX_ID_LEN; i++)
+    {
+        if (cmp(word, keywords[i]))
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int valid(char word[], int length)
+{
+    if (!is_alfa(word[0]))
     {
         return 0;
     }
-    for(int i = 0;i < length;i++)
+    for (int i = 0; i < length; i++)
     {
-        if(!is_alfanum(a))
+        if (!is_alfanum(word[1]))
         {
             return 0;
         }
@@ -62,38 +73,48 @@ int valid(char word[],int length)
 
 int find_idents(FILE *stream)
 {
+    int indents = 0;
     char word[2048];
-    while (fscanf(stream, " %2047s", word) == 1) 
+    while (fscanf(stream, " %2047s", word) == 1)
     {
-        
+        if (!is_keyword(word) && valid(word, 2048))
+        {
+            indents++;
+        }
     }
 }
 
-int cmp(const void* first_arg, const void* second_arg) {
-	char *a = *(char**)first_arg;
-	char *b = *(char**)second_arg;
-	return strcmp(a, b);
+int cmp(const void *first_arg, const void *second_arg)
+{
+    char *a = *(char **)first_arg;
+    char *b = *(char **)second_arg;
+    return strcmp(a, b);
 }
 
-int index_cmp(const void* first_arg, const void* second_arg) {
-	int a = *(int*)first_arg;
-	int b = *(int*)second_arg;
-	return strcmp(tab[a], tab[b]);
+int index_cmp(const void *first_arg, const void *second_arg)
+{
+    int a = *(int *)first_arg;
+    int b = *(int *)second_arg;
+    return strcmp(tab[a], tab[b]);
 }
 
-int main(void) {
-	char file_name[40];
-	FILE *stream;
+int main(void)
+{
+    char file_name[40];
+    FILE *stream;
 
-  if(TEST)  stream = stdin;
-  else {
-      scanf("%s",file_name);
-      stream = fopen(file_name,"r");
-      if(stream == NULL) {
-          printf("fopen failed\n");
-          return -1;
-      }
-  }
-	printf("%d\n", find_idents(stream));
-	return 0;
+    if (TEST)
+        stream = stdin;
+    else
+    {
+        scanf("%s", file_name);
+        stream = fopen(file_name, "r");
+        if (stream == NULL)
+        {
+            printf("fopen failed\n");
+            return -1;
+        }
+    }
+    printf("%d\n", find_idents(stream));
+    return 0;
 }
