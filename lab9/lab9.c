5bae9e93e630a56b1db64575268a2b3ebc8e92f8
Hubjerd <hubi.hey@vp.pl>
2024-05-13 13:18:04 +0200

    Done lab 9

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -5,6 +5,7 @@
 
 #define NULLTERMINATOR 0
 #define NEWLINE '\n'
+
 #define TAB_SIZE  1000
 #define BUF_SIZE  1000
 
@@ -15,22 +16,22 @@ int get(int cols, int row, int col, const int *A)
 
 void set(int cols, int row, int col, int *A, int value) 
 {
-    A[row * clos + col] = value;
+    A[row * cols + col] = value;
 }
 
 void prod_mat(int rowsA, int colsA, int colsB, int *A, int *B, int *AB)
 {
 	int sum;
-    for(int i = 0;i < rowsA;i < colsA;i++)
+    for(int i = 0;i < rowsA;i++)
     {
-        for(int j = 0;j < colsA;j++)
+        for(int j = 0;j < colsB;j++)
         {
 			sum = 0;
-            for(int k = 0;k < closB;k++)
+            for(int k = 0;k < colsA;k++)
             {
-                sum += get(colsA,i,k,A) * get(colsB,k,j,B));
+                sum += get(colsA,i,k,A) * get(colsB,k,j,B);
             }
-			set(colsA,i,j,AB,sum);
+			set(rowsA,i,j,AB,sum);
         }
     }
 }
@@ -52,7 +53,7 @@ void print_mat(int rows, int cols, int *t)
     {
         for(int j = 0;j < cols;j++)
         {
-            printf("%d",t[i * cols + j]);
+            printf("%d ",t[i * cols + j]);
         }
 		printf("\n");
     }
@@ -61,20 +62,15 @@ void print_mat(int rows, int cols, int *t)
 int read_char_lines(char *array[]) 
 {    
 	char buffer[BUF_SIZE] = {0};
-    int lines_counter = 0;
-
+    int lines_counter = 0, length;
     while(!feof(stdin)){
+		length = strlen(stdin);
         fgets(buffer, BUF_SIZE, stdin);
-        int length = strlen(buffer);
-
         array[lines_counter] = malloc((length + 1) * sizeof(char));
         memcpy(array[lines_counter], buffer, length + 1);
-
         lines_counter++;
     }
-
     array[lines_counter] = NULL;
-
     return lines_counter;
 }
 
@@ -92,21 +88,60 @@ void delete_lines(char *array[])
 	}
 }
 
-int read_int_lines_cont(int *ptr_array[]) 
-{
-    int i = 0;
-    while(ptr_array[i])
-    {
-        int *x = (int*)malloc(sizeof(int));
-        for(int j = 0;)
+int* read_int_line(int* size){
+    int* line = NULL, result, buffer_counter = 0;
+    *size = 0;
+    char buffer[TAB_SIZE] = {0}, c = fgetc(stdin);
+
+    while(!feof(stdin) && c != '\n'){
+        if(c == ' '){
+            result = atoi(buffer);
+            memset(buffer, 0, BUF_SIZE);
+            line = realloc(line, (*size + 1)*sizeof(int));
+            line[*size] = result;
+            buffer_counter = 0;
+            (*size)++;
+        } else {
+            buffer[buffer_counter] = c;
+            buffer_counter++;
+        }
+
+        c = fgetc(stdin);
     }
+
+    if(buffer_counter == 0)
+        return NULL;
+    result = atoi(buffer);
+    line = realloc(line,  (*size + 1)*sizeof(int));
+    line[*size] = result;
+    (*size)++;
+    return line;
 }
 
-void write_int_line_cont(int *ptr_array[], int n) 
+int read_int_lines_cont(int *ptr_array[]) {
+    int elements_counter = 0, lines_counter = 0,* tab = ptr_array[0], line_size,* line_array;
+    while(!feof(stdin)){
+        line_size = 0;
+        line_array = read_int_line(&line_size);
+        if(line_array == NULL)
+            break;
+        memcpy(&tab[elements_counter], line_array, sizeof(int)*line_size);
+        free(line_array);
+        ptr_array[lines_counter] = &tab[elements_counter];
+        elements_counter += line_size;
+        lines_counter++;
+    }
+    ptr_array[lines_counter] = ptr_array[0] + elements_counter;
+    return lines_counter;
+}
+
+void write_int_line_cont(int *ptr_array[], int n)
 {
-    for(int i = 0;i < n;i++)
-    {
-        printf("%d",ptr_array[i]);
+    int* temp_ptr = ptr_array[n];
+
+    while(temp_ptr != ptr_array[n + 1]){
+        printf("%i ", *temp_ptr);
+        temp_ptr++;
     }
 }
 
@@ -121,34 +156,41 @@ int read_int_lines(line_type lines_array[])
 	int lines_count = 0,line_length,*line_array;
 	while(!feof(stdin))
 	{
-		line_size = 0;
-		line_array = read_int_lines(&line_size);
+		line_length = 0;
+		line_array = read_int_lines(&line_length);
 		if(line_array == NULL)
 		{
 			return lines_count;
 		}
-		line_array[lines_count].values = malloc(sizeof(int)*line_size);
-		line_array[lines_count].len = line_size;
-
+		lines_array[lines_count].values = malloc(sizeof(int)*line_length);
+		lines_array[lines_count].len = line_length;
 	}
 	return lines_count;
 }
 
-void write_int_line(line_type lines_array[], int n) {
+void write_int_line(line_type lines_array[], int n) 
+{
+    for(int i = 0; i < lines_array[n].len; i++){
+        printf("%i ", lines_array[n].values[i]);
+    }
+    printf("\n%.2lf \n", lines_array[n].average);
 }
 
-void delete_int_lines(line_type array[], int line_count) {
+void delete_int_lines(line_type array[], int line_count) 
+{
+    for(int i = 0; i < line_count; i++)
+        free(array[i].values);
 }
 
 int cmp (const void *a, const void *b) 
 {
-	line_type a = *((line_type*)_a);
-	line_type b = *((line_type*)_b);
-	if(a.average < b.average)
+	line_type first = *((line_type*)a);
+	line_type second = *((line_type*)b);
+	if(first.average < second.average)
 	{
 		return -1;
 	}	
-	else if(a.average > b.average)
+	else if(first.average > second.average)
 	{
 		return 1;
 	}
@@ -160,7 +202,7 @@ int cmp (const void *a, const void *b)
 
 void sort_by_average(line_type lines_array[], int line_count) 
 {
-	qsort(line_array,line_count,sizeof(line_type),cmp);
+	qsort(lines_array,line_count,sizeof(line_type),cmp);
 }
 
 typedef struct {
@@ -213,7 +255,7 @@ void make_CSR(triplet *triplet_array, int n_triplets, int rows, int *V, int *C, 
 	{
 		V[i] = triplet_array[i].v;
 		C[i] = triplet_array[i].c;
-		for(int j = triplet_array.r + 1;j < rows + 1;k++)
+		for(int j = triplet_array[i].r + 1;j < rows + 1;j++)
 		{
 			R[j] += 1;
 		}
